da7851aca29ec1c54ef9f65e9d1c5c1df0550cc0
ebisinteidennis <ebisinteidennis@gmail.com>
2022-10-28 20:03:11 +0100

    Adding settings page

--- twitterclonefile/routes.js
+++ twitterclonefile/routes.js
@@ -1,5 +1,7 @@
 const displayContent  = (req, res) => {
     const url = req.url;
+    const method = req.method;
+
     if (url == "/profile")
     {
         res.setHeader("Content-Type", "text/html");
@@ -13,6 +15,13 @@ const displayContent  = (req, res) => {
         return res.end();
     } 
     
+    else if (url == "/settings")
+    {    
+        res.setHeader("Content-Type", "text/html");
+        res.write("<h1>Settings page</h1>");
+        return res.end();
+    } 
+
     res.setHeader("Content-Type", "text/html");
     res.write("<p>404 page not found</p>");
     res.end();
