ca85db1ec5700186da896f0f6b7d61b11ee1c4e8
ebisinteidennis <ebisinteidennis@gmail.com>
2022-10-29 13:39:18 +0100

    express server

--- TwitterClone/app.js
+++ TwitterClone/app.js
@@ -2,3 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3003;
 const server = app.listen(port, ()=> console.log("Server listening on port" + port));
+
+app.get("/",(req, res, next) =>
+{
+    res.status(200).send("yahoo!");
+
+} )
