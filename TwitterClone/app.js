b540d2fe861428412df55fd6ae0cd0e80d641636
ebisinteidennis <ebisinteidennis@gmail.com>
2022-10-29 14:50:06 +0100

    Middleware Redirecting the user if not logged in

--- TwitterClone/app.js
+++ TwitterClone/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3003;
 const server = app.listen(port, ()=> console.log("Server listening on port" + port));
+const middleware = require('./middleware')
 
 app.set("view engine", "pug");
 app.set("views", "views");//pug
-app.get("/",(req, res, next) =>
+
+app.get("/",middleware.requireLogin,(req, res, next) =>
 {
     var payload = {
         pageTitle: "Home"
