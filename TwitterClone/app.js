faa88f7db38055c8993a818aac221a874e18fe0d
ebisinteidennis <ebisinteidennis@gmail.com>
2022-10-29 13:47:36 +0100

    working with template files 'pug'

--- TwitterClone/app.js
+++ TwitterClone/app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3003;
 const server = app.listen(port, ()=> console.log("Server listening on port" + port));
 
+app.set("view engine", "pug");
+app.set("views", "views");//pug
 app.get("/",(req, res, next) =>
 {
     res.status(200).send("yahoo!");
