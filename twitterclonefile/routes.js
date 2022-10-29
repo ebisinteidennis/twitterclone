bd88fa638e332abcf855aa6c4daf492787363d36
ebisinteidennis <ebisinteidennis@gmail.com>
2022-10-29 12:15:09 +0100

    Adding post method in the settings page

--- twitterclonefile/routes.js
+++ twitterclonefile/routes.js
@@ -15,10 +15,17 @@ const displayContent  = (req, res) => {
         return res.end();
     } 
     
+    else if (url == "/settings" && method=="POST")
+    {    
+        res.setHeader("Content-Type", "text/html");
+        res.write("<h1>Form was submitted</h1><form><label></label></form>");
+        return res.end();
+    }
+    
     else if (url == "/settings")
     {    
         res.setHeader("Content-Type", "text/html");
-        res.write("<h1>Settings page<h1>);
+        res.write("<h1>The input element</h1><form><label></label></form>");
         return res.end();
     } 
 
