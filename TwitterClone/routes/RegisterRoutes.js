579c719912bf6659bf112c235183b46efffab2d5
ebisinteidennis <ebisinteidennis@gmail.com>
2022-11-01 19:54:48 +0100

    Checking if the username or email are already in use

--- TwitterClone/routes/RegisterRoutes.js
+++ TwitterClone/routes/RegisterRoutes.js
@@ -1,45 +1,64 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser")
 const User = require('../schemas/UserSchema');
 
 app.set("view engine", "pug");
-app.set("views", "views");//pug
-app.use(bodyParser.urlencoded({extended: false}));
+app.set("views", "views");
 
-router.get("/",(req, res, next) =>
-{
+app.use(bodyParser.urlencoded({ extended: false }));
+
+router.get("/", (req, res, next) => {
+
     res.status(200).render("register");
-
 })
 
-router.post("/", async (req, res, next) =>
-{   
+router.post("/", async (req, res, next) => {
+
     var firstName = req.body.firstName.trim();
-    var lasttName = req.body.lastName.trim();
-    var userName = req.body.userName.trim();
+    var lastName = req.body.lastName.trim();
+    var username = req.body.username.trim();
     var email = req.body.email.trim();
     var password = req.body.password;
-    
+
     var payload = req.body;
 
-    if(firstName && lasttName && email && password){
-        var User = await User.findOne({
+    if(firstName && lastName && username && email && password) {
+        var user = await User.findOne({
             $or: [
-                {username: username},
-                {email: email},
+                { username: username },
+                { email: email }
             ]
         })
-        console.log(User);
-        console.log("Hello");
+        .catch((error) => {
+            console.log(error);
+            payload.errorMessage = "Something went wrong.";
+            res.status(200).render("register", payload);
+        });
+
+        if(user == null) {
+            // No user found
+
+        }
+        else {
+            // User found
+            if (email == user.email) {
+                payload.errorMessage = "Email already in use.";
+            }
+            else {
+                payload.errorMessage = "Username already in use.";
+            }
+            res.status(200).render("register", payload);
+        }
+        
+        
+
     }
     else {
-        payload.console.errorMessage = "Make sure each field as a valid value";
+        payload.errorMessage = "Make sure each field has a valid value.";
         res.status(200).render("register", payload);
     }
-
-    
 })
 
 module.exports = router;
