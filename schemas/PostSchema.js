e666b4026b7b3554797b6d4e840ce226dc165e9e
ebisinteidennis <ebisinteidennis@gmail.com>
2022-11-08 09:12:41 +0100

    Creating the post Mongoose schema

---
+++ schemas/PostSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    content: {type: String, trim: true},
+    postedby: {type: Schema.Types.ObjectId, ref: "User"},
+    pinned: Boolean
+    
+},{timestamps: true} );
+
+var Post = mongoose.model('Post', PostSchema)
+module.exports = Post;
