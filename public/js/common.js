240a8f88c8d8495528d10266ade02a93053af647
ebisinteidennis <ebisinteidennis@gmail.com>
2022-11-09 20:00:46 +0100

    Styling the comment, retweeting and like buttons

--- public/js/common.js
+++ public/js/common.js
@@ -42,7 +42,7 @@ function createPostHtml(postData) {
                 </div>
                 <div class='postContentContainer'>
                     <div class='header'>
-                        <a href='/profile/@${postedBy.username}'>${displayName}</a>
+                        <a href='/profile/@${postedBy.username}' class='displayName'>${displayName}</a>
                         <span class='username'>${postedBy.username}</spa>
                         <span class='date'>${timestamp}</spa>
                     </div>
@@ -50,6 +50,24 @@ function createPostHtml(postData) {
                         <span>${postData.content}</span>
                     </div>
                     <div class='postFooter'>
+                        <div class='postButtonContainer'>
+                            <button>
+                                <i class='far fa-comment'></i>
+                            </button>
+                        </div>
+
+                        <div class='postButtonContainer'>
+                            <button>
+                                <i class='fas fa-retweet'></i>
+                            </button>
+                        </div>
+
+                        <div class='postButtonContainer'>
+                            <button>
+                                <i class='far fa-heart'></i>
+                            </button>
+                        </div>
+
                     </div>
                 </div>
             </div>
