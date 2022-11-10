48c1936155e0c17d70ef70247e9c0dbd43a57158
ebisinteidennis <ebisinteidennis@gmail.com>
2022-11-10 16:53:46 +0100

    Adding the likes field to the schemas

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -9,7 +9,8 @@ const UserSchema = new Schema({
     email: {type: String, required: true, trim: true, unique: true},
     password: {type: String, required: true},
     profilePic: {type: String, default: "/images/profilePic.jpeg"},
-},{timestamps: true} );
+    likes: [{type: Schema.Types.ObjectId, ref: "Post"}]
+},{timestamps: true} ); 
 
 var User = mongoose.model('User', UserSchema)
 module.exports = User;
