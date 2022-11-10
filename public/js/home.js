7b4b50a36344077e2f194bbc07a39a600b009ec2
ebisinteidennis <ebisinteidennis@gmail.com>
2022-11-10 11:51:34 +0100

    outputting the post on the page

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,17 @@
 $(document).alert(()=>{
     $.get("/api/posts", results=> {
-        console.log(results);
+        outputPosts(results, $(".postsContainer"));
     })
 })
+
+function outputPosts(results, container) {
+    container.html("");
+    //loop over every single results
+     results.array.forEach(result => {
+        var html = createPostHtml(result)
+        container.append(html);
+    });
+    if (results.length == 0) {
+        container.append("<span cass='noResults'>Nothing to show.</span>")
+    }
+}
