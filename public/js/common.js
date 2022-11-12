f60f6272a7abab48d12023ecea7f71ddd2109eec
ebisinteidennis <ebisinteidennis@gmail.com>
2022-11-12 16:01:46 +0100

    showing the number of retweets next to the button

--- public/js/common.js
+++ public/js/common.js
@@ -64,14 +64,13 @@ $(document).on("click", ".retweetButton", (event) => {
         url: `/api/posts/${postId}/retweet`,
         type: "POST",
         success: (postData) => {
-            console.log(postData);
-            // button.find("span").text(postData.likes.length || "");
+            button.find("span").text(postData.retweetUsers.length || "");
             
-            // if(postData.likes.includes(userLoggedIn._id)) {
-            //     button.addClass("active");
-            // }
-            // else{button.removeClass("active");
-            // }
+            if(postData.retweetUsers.includes(userLoggedIn._id)) {
+                button.addClass("active");
+            }
+            else{button.removeClass("active");
+            }
         }
     })
 
@@ -125,6 +124,7 @@ function createPostHtml(postData) {
                             <div class='postButtonContainer green'>
                                 <button class='retweetButton'>
                                     <i class='fas fa-retweet'></i>
+                                    <span>${postData.retweetUsers.length || ""}</span>
                                 </button>
                             </div>
                             <div class='postButtonContainer red'>
