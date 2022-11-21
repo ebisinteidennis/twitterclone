3082773f5825689d86b87e150cc2337b418464f6
ebisinteidennis <ebisinteidennis@gmail.com>
2022-11-21 10:22:52 +0100

    adding the delete button to our own posts

--- public/js/common.js
+++ public/js/common.js
@@ -172,6 +172,11 @@ function createPostHtml(postData, largeFont = false) {
 
     }
 
+    var buttons = "";
+    if (postData.postedBy._id == userLoggedIn._id) {
+        buttons = `<button data-id="${postData._id}" data-toggle="modal" data-target="#deletePostModal"><i class='fas fa-times'></i></button>`;
+    }
+
     return `<div class='post ${largeFontClass}' data-id='${postData._id}'>
                 <div class='postActionContainer'>
                     ${retweetText}
@@ -185,6 +190,7 @@ function createPostHtml(postData, largeFont = false) {
                             <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
                             <span class='username'>@${postedBy.username}</span>
                             <span class='date'>${timestamp}</span>
+                            ${buttons}
                         </div>
                         ${replyFlag}
                         <div class='postBody'>
